0592956e0ca95bd47b17be178451734378c5b188
atluribhogendra <31948581+atluribhogendra@users.noreply.github.com>
2020-02-17 17:38:50 -0600

    Modifying OceanMap resolving errors

--- Assignment2/src/OceanMap.java
+++ Assignment2/src/OceanMap.java
@@ -1,7 +1,6 @@
 import java.awt.Point;
 import java.util.Random;
 public class OceanMap{
-<<<<<<< HEAD
 	int x,y;
 	private static OceanMap oceanMap = null;
 	
@@ -38,52 +37,14 @@ public class OceanMap{
 		}
 		return oceanMap;
 	}
-=======
-int x,y;
-private static OceanMap oceanMap = null;
-
-Random rand = new Random();
-boolean[][] oceanGrid = new boolean[10][10];
-public OceanMap() {
-int noOfIslands=0;
-   
-    while(noOfIslands<10) {
-    x=rand.nextInt(10);
-        y=rand.nextInt(10);
-        if(!oceanGrid[x][y]) {
-        oceanGrid[x][y]=true;
-        noOfIslands++;
-        continue;
-        }
-    }
-   
-}
-//     for(int i=0;i<10;i++) {
-// for(int j=0;j<10;j++) {
-// System.out.print(oceanGrid[i][j]);
-// }
-// System.out.println();
-// }
-// }
-public boolean[][] getMap() {
-return oceanGrid;
-}
-
-public static OceanMap getInstance() {
-if(oceanMap==null) {
-oceanMap=new OceanMap();
-}
-return oceanMap;
-}
->>>>>>> origin/master
 
     public Point getShipLocation(){
-x = rand.nextInt(10); // Generating a random number from 0 -10
-y = rand.nextInt(10);
+		x = rand.nextInt(10);		// Generating a random number from 0 -10
+		y = rand.nextInt(10);
         return new Point(x,y);
 
     }
-   
-   
+    
+    
 
 }
