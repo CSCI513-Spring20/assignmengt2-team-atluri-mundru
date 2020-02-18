6a525f81b30a84abe87b86e8d964009ac2f10ed7
sandeepmundru1 <59979990+sandeepmundru1@users.noreply.github.com>
2020-02-17 21:17:40 -0600

    Add files via upload
    added pirateship code and enhancements in oceanmap class

--- Assignment2/src/PirateShip.java
+++ Assignment2/src/PirateShip.java
@@ -1,4 +1,60 @@
+import java.awt.Point;
+import java.util.Observable;
+import java.util.Observer;
+public class PirateShip implements Observer {
+int xc;
+int yc;
+OceanMap oceanMap = OceanMap.getInstance();
+Point psl=oceanMap.getPirateShipLocation();//psl means pirateshiplocation
+Point csl;
+public PirateShip(int x, int y) {
+xc = x;
+yc =y;
+}
 
-public class PirateShip {
-	
+
+public Point getPirateShipLocation() {
+return new Point(xc,yc);
+}
+@Override
+public void update(Observable o, Object arg) {
+if(o instanceof Ship) {
+csl=((Ship)o).getShipLocation();//csl means currentshiplocation of columbus ship
+updatePirateShip();
+}
+}
+
+public void updatePirateShip() {
+int cship_x=csl.x;
+int cship_y = csl.y;
+int pirateship_x = psl.x;
+int pirateship_y = psl.y;
+if(pirateship_x!=9 && pirateship_x - cship_x <0) {
+if(!oceanMap.getMap()[pirateship_x+1][pirateship_y]) {
+pirateship_x=pirateship_x+1;
+
+}
+
+}
+else if(pirateship_x!=0 && pirateship_x - cship_x >0) {
+if(!oceanMap.getMap()[pirateship_x-1][pirateship_y]) {
+pirateship_x=pirateship_x-1;
+}
+
+
+
+}
+if(pirateship_y!=9 && pirateship_y - cship_y<0) {
+if(!oceanMap.getMap()[pirateship_x][pirateship_y+1]) {
+pirateship_y=pirateship_y+1;
+}
+}
+else if(pirateship_y!=0 && pirateship_y -cship_y >0) {
+if(!oceanMap.getMap()[pirateship_x][pirateship_y-1]) {
+pirateship_y=pirateship_y-1;
+}
+}
+
+
+}
 }
